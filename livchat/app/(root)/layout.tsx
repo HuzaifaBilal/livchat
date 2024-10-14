413a9a34edc1e7745a1ddabda171206af9ae9d51
Huzaifa Bilal <huzaifabilal2191@gmail.com>
2024-10-14 21:17:48 +0530

    lc-56 feat loading logo in friends

--- livchat/app/(root)/layout.tsx
+++ livchat/app/(root)/layout.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import SidebarWrapper from "@/components/shared/sidebar/SidebarWrapper";
+import LoadingLogo from "@/components/shared/LoadingLogo";
 
 type Props = React.PropsWithChildren<{}>;
 
 const layout = ({ children }: Props) => {
-  return <SidebarWrapper>{children}</SidebarWrapper>;
+  {
+    children ? (
+      <SidebarWrapper>{children}</SidebarWrapper>
+    ) : (
+      <LoadingLogo></LoadingLogo>
+    );
+  }
 };
 
 export default layout;
