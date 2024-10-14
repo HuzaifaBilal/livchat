1ff19951b8ffe5280aa29c0962f17b0ac6c4715d
Huzaifa Bilal <huzaifabilal2191@gmail.com>
2024-10-14 20:48:17 +0530

    lc-52 loading templates added

---
+++ livchat/app/(root)/friends/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingLogo from "@/components/shared/LoadingLogo";
+import React from "react";
+
+type Props = {};
+
+const loading = (props: Props) => {
+  return <LoadingLogo />;
+};
+
+export default loading;
